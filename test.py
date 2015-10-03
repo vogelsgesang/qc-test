e8b6e09e9a0aa9228227cdb44f15bf19bf1f5954
Christoph Neumann <programmdesign@users.noreply.github.com>
2015-10-03 16:22:42 +0200

    Update test.py

--- test.py
+++ test.py
@@ -45,3 +45,14 @@ eval("print variable")
 print "das %s" % "ist"
 eval("print variable")
 eval("print variable")
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
