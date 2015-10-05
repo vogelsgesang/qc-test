ab3c2e0918d9fad4eea106f135c32568b763e9d5
Christoph Neumann <programmdesign@users.noreply.github.com>
2015-10-05 16:08:45 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -151,3 +151,310 @@ eval("print variable")
 eval("print variable")
 
 variable = "testing"
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+
+
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+
+variable = "testing"
