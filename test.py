3656a9ad9f45d907903ef91f934f7fe661e8ba55
Christoph Neumann <programmdesign@users.noreply.github.com>
2015-10-05 17:03:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
 variable = "testing"
 print "das ist ein %s" % variable
 print "das ist ein %s" % variable
