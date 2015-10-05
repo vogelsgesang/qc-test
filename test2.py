6f83fde0b1b09d9b3d57aa3bdb562c69eec88f25
Christoph Neumann <programmdesign@users.noreply.github.com>
2015-10-05 16:20:57 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -18,6 +18,23 @@ exec(variable)
 eval(varialbe)exec(variable)
 eval(varialbe)
 eval(variable)
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)exec(variable)
+eval(varialbe)
+eval(variable)
 eval("print variable")
 print "das %s" % "ist"
 eval("print variable")
