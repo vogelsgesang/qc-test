d4efb862b8c6bb914f04af2f8045a6efb5278d2b
Cody <cody@quantifiedcode.com>
2016-01-12 13:51:07 +0000

    Migrated `%` string formating

--- b.py
+++ b.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 exec(variable)
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
 eval("print variable")
 eval("print variable")
 
